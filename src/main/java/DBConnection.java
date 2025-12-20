1358fbd230aac489db1c505ba640781519695e32
Toavina051 <hei.toavina.2@gmail.com>
2025-12-20 21:02:39 +0300

    feat:add file DBConnection

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    String url;
+    String user;
+    String password;
+
+    public DBConnection() {
+        this.url = "jdbc:postgresql://localhost:5432/mini_dish_db";
+        this.user = "mini_dish_db_manager";
+        this.password = "123456";
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+}
